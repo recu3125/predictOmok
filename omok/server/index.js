fa6da03114bbcc0aaf2737aa972bf7d2fc4dae79
Patrick3125 <3125patrick@gmail.com>
2023-12-03 22:28:14 -0800

    some frontend on joing rooms

--- omok/server/index.js
+++ omok/server/index.js
@@ -181,6 +181,21 @@ const broadcastboardID = (boardId) => {
 
 const express = require('express')
 const app = express()
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Specify the allowed HTTP methods
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Specify the allowed headers
+
+  // Allow credentials (if needed)
+  // res.header('Access-Control-Allow-Credentials', true);
+
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
 app.get('/api/boards', function (req, res) {
   res.send(JSON.stringify(boards))
 })
